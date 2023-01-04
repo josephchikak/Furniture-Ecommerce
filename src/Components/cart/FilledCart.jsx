a34461350ed2373ea323ca8c5c6a0b12387219a0
raytheboffin <111377374+josephchikak@users.noreply.github.com>
2023-01-04 13:33:34 +0200

    fixing dependencies

--- src/Components/cart/FilledCart.jsx
+++ src/Components/cart/FilledCart.jsx
@@ -1,6 +1,7 @@
-import { Typography, Button, Grid} from '@mui/material'
-import CartItem from './CartItem'
-import {Link} from 'react-router-dom'
+import React from 'react';
+import { Typography, Button, Grid} from '@mui/material';
+import CartItem from './CartItem';
+import {Link} from 'react-router-dom';
 
 
 
