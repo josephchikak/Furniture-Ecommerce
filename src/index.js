50b7f12b25a09bf000947fdb6b27ff96e7295099
raytheboffin <111377374+josephchikak@users.noreply.github.com>
2023-01-05 13:44:25 +0200

    adding 404.html

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter basename="/Furniture-Ecommerce">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
