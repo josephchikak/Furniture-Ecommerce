03731b6ec0f51432e626f929226994e3f9ebf63d
raytheboffin <111377374+josephchikak@users.noreply.github.com>
2023-01-07 22:21:43 +0200

    emoty cart after chekout

--- src/Components/cart/FilledCart.jsx
+++ src/Components/cart/FilledCart.jsx
@@ -20,6 +20,7 @@ export const FilledCart = ({cart, handleCartUpdate, handleRemove, emptyCart}) =>
         if (res.ok) return res.json()
         return res.json().then(json => Promise.reject(json))
     }).then(({url}) =>{
+        emptyCart()
         window.location = url
     }).catch(e =>{
         console.error(e.error)
